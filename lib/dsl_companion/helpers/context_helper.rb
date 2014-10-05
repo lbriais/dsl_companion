124891b5e40c5d9dff3bacb3fa3930fb7af0f465
Laurent B <lbnetid+gh@gmail.com>
2014-10-05 15:54:36 +0200

    Minor

--- lib/dsl_companion/helpers/context_helper.rb
+++ lib/dsl_companion/helpers/context_helper.rb
@@ -2,17 +2,6 @@ module DSLCompanion
 
   module ContextHelper
 
-
-    # def with_dictionary &block
-    #   @within_dictionary = true
-    #
-    #   raise "'with_dictionary' cannot be processed in this context !" unless interpreter?
-    #   execute_within_context @context, &block
-    # ensure
-    #   @within_dictionary = false
-    # end
-
-
     def execute_within_context(context=@context, &block)
       # Execute the block if any
       if block_given?
@@ -28,29 +17,6 @@ module DSLCompanion
         end
       end
     end
-
-
-
-
-    #  def within_dictionary?
-    #    if interpreter?
-    #      @within_dictionary
-    #    else
-    #      @interpreter.within_dictionary?
-    #    end
-    # end
-
-    # 
-
-
-    # def interpreter_exec_mode mode=nil
-    #   if mode.nil?
-    #     @interpreter.exec_mode
-    #   else
-    #     @interpreter.exec_mode = mode
-    #   end
-    # end
-
 
   end
 
