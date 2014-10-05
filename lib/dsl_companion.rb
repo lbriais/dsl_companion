217d29b5c2ad1608060bc3e357b25b82775895a1
Laurent B <lbnetid+gh@gmail.com>
2014-10-05 19:24:50 +0200

    Removed useless dependency

--- lib/dsl_companion.rb
+++ lib/dsl_companion.rb
@@ -1,6 +1,5 @@
 require 'dsl_companion/version'
 require 'dsl_companion/meta_helper'
-require 'dsl_companion/helpers/context_helper'
 require 'dsl_companion/helpers/active_record'
 
 # Preloaded features
